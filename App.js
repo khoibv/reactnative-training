cd5d16dad7ccbf2230bfb742b26b31cf6a1c7a4d
Khoi Bui Van <khoibv@nissho-vn.com>
2020-06-02 19:41:35 +0700

    Flex layout

--- App.js
+++ App.js
@@ -11,22 +11,15 @@ import {StyleSheet, Text, View} from 'react-native';
 
 const App: () => React$Node = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Hi ReactNative</Text>
+    <View style={{flex: 1}}>
+      <View style={{alignItems: 'center'}}>
+        <Text style={{color: 'blue', fontSize: 32}}>Hi ReactNative</Text>
+      </View>
+      <View style={{flex: 1, backgroundColor: '#E93E43'}} />
+      <View style={{flex: 2, backgroundColor: '#F5A942'}} />
+      <View style={{flex: 3, backgroundColor: '#4EBC7A'}} />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  welcome: {
-    color: '#FF0000',
-    fontSize: 32,
-  },
-});
 
 export default App;
